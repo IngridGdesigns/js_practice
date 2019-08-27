647bc2126c065d83b0f58ae0ba7139c0e0660158
IngridGdesigns <aleja.morada@gmail.com>
2019-08-26 18:12:32 -0700

    let and var Variables

--- webpack-starter/src/scripts/index.js
+++ webpack-starter/src/scripts/index.js
@@ -1,3 +1,20 @@
 
 
 console.log('this stuff is working now');
+
+
+const cookieId = 34; //needs to be initialize, its declared as a constant
+
+console.log(cookieId);
+
+//let has block scoping and var doesnt't
+if(true){
+    let cookie = 'chocolate chip';
+}
+//console.log(cookie); - error will appear
+
+//var doesnt't have block scoping, could lead you to errors
+if(true){
+    var cookies = 'yummm';//
+}
+console.log(cookies);
