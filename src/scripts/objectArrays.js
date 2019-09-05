24b59317772b6088a56063934d4d72e4a2ccf3c7
IngridGdesigns <aleja.morada@gmail.com>
2019-09-05 16:07:41 -0700

    added findIndex to file

--- src/scripts/objectArrays.js
+++ src/scripts/objectArrays.js
@@ -1,14 +1,13 @@
 //Constructor Function is used to instantiate objects
 
-
 /* Example 1 */
 //primary way to create objects using functions
 function Cat(id) {
-    this.catId = id; //this stores the property id
-    console.log(this); //this keyword access the content of the object
+  this.catId = id; //this stores the property id
+  console.log(this); //this keyword access the content of the object
 }
 
-let kitty = new Cat('meeoooww'); //brand new empty objects
+let kitty = new Cat("meeoooww"); //brand new empty objects
 console.log(kitty); //Cat { catId: 'meeoooww' }
 
 ////////////////////////////////////////////////////////////////
@@ -16,15 +15,16 @@ console.log(kitty); //Cat { catId: 'meeoooww' }
 //Prototypes - basics
 
 /* Example 1 */
-function Bird(song){
-    this.birdSong = song; //consturctor function is simple
+function Bird(song) {
+  this.birdSong = song; //consturctor function is simple
 }
 
-Bird.prototype.sing = function(){ //we can add methods directly on prototype
-    console.log('The bird sings: ' + this.birdSong);
+Bird.prototype.sing = function() {
+  //we can add methods directly on prototype
+  console.log("The bird sings: " + this.birdSong);
 };
 
-let parrot = new Bird('lalalalalLAAAA');
+let parrot = new Bird("lalalalalLAAAA");
 parrot.sing();
 
 ////////////////////////////////////////////////////////////////
@@ -33,9 +33,9 @@ parrot.sing();
 
 /* Example  1*/
 String.prototype.hello = function() {
-    return this.toString() + ' Hello ';
-}
-console.log('foo'.hello()); //foo Hello
+  return this.toString() + " Hello ";
+};
+console.log("foo".hello()); //foo Hello
 
 ////////////////////////////////////////////////////////////////
 
@@ -52,14 +52,14 @@ and JSON.parse to convert JSON back into an object
 //JSON Stringify
 /* Example 1 */
 let lunch = {
-    beverage: 'water',
-    maincourse: 'ham and cheese',
-    sidedish: 'ceasar salad',
-    dessert: 'mango mochi'
+  beverage: "water",
+  maincourse: "ham and cheese",
+  sidedish: "ceasar salad",
+  dessert: "mango mochi"
 };
 
 let lunchJson = JSON.stringify(lunch); //takes object and converts into a string
-console.log(lunchJson); 
+console.log(lunchJson);
 //{"beverage":"water","maincourse":"ham and cheese","sidedish":"ceasar salad","dessert":"mango mochi"}
 
 /* JSON objects have several important differences from object literals
@@ -72,11 +72,11 @@ See example 2
 
 /* Example 2 */
 let meetup = {
-    title: 'Conference',
-    participants: ['WSC', 'Github']
+  title: "Conference",
+  participants: ["WSC", "Github"]
 };
 
-let meetupJson = JSON.stringify(meetup, null, 2);//space argument is telling JS to 
+let meetupJson = JSON.stringify(meetup, null, 2); //space argument is telling JS to
 //show nested objects on mutliple lines, w/indentation of 2 spacxes inside an object
 console.log(meetupJson);
 //Output: isin't it pretty??
@@ -89,56 +89,60 @@ console.log(meetupJson);
 //    }
 //the null holds a place for a function
 
-
 let room = {
-    number: 23 
-  };
-  
-  let party = {
-    title: "Dance time",
-    date: new Date(Date.UTC(2017, 0, 1)),
-    room, //automatically calls if it is available room ={ number: 23 }
-  };
-  
- console.log( JSON.stringify(party) );
+  number: 23
+};
 
- //JSON PARSE
- let jsonIn = `[{"may": 5}, {"june": 6} ]`
- let months = JSON.parse(jsonIn);
- console.log(months); //parsed into a regualar object notation
+let party = {
+  title: "Dance time",
+  date: new Date(Date.UTC(2017, 0, 1)),
+  room //automatically calls if it is available room ={ number: 23 }
+};
+
+console.log(JSON.stringify(party));
+
+//JSON PARSE
+let jsonIn = `[{"may": 5}, {"june": 6} ]`;
+let months = JSON.parse(jsonIn);
+console.log(months); //parsed into a regualar object notation
 ////////////////////////////////////////////////////////////////
 
 //Array Iteration
 
 /* Example  1*/
-// Using animalID we will explore various methods to iterate through arrays
+// Using animalID we will explore various methods to iterate through arrays in place of a for loop
 let animalID = [
-    { petID: 123, dog: 'big', color: 'black'},
-    { petID: 43, cat: 'small', color: 'orange'},
-    { petID: 378, bird: 'tiny', color: 'blue'}
+  { petID: 123, dog: "big", color: "black" },
+  { petID: 43, cat: "small", color: "orange" },
+  { petID: 378, bird: "tiny", color: "blue" }
 ];
 
 //forEach
-animalID.forEach( (color) => console.log(color));//calls a function on each element on the array
+animalID.forEach(color => console.log(color)); //calls a function on each element on the array
 
 
 //Filter
 //loop through the entire array, returns filtered new array  cat:203, color: orange`
-let animalList = animalID.filter(
-    animal => animal.color === 'orange'
-);
-console.log(animalList); 
+let animalList = animalID.filter(animal => animal.color === "orange");
+console.log(animalList);
 
 
-//Every 
-let animalresults = animalID.every(
-    pet => pet.petID > 10
-);
+//Every
+let animalresults = animalID.every(pet => pet.petID > 10);
 console.log(animalresults); //returns true, every pet Id is greater than 10
+//we only get one true or false back
 
 
 //Locate the first Match
 let pet = animalID.find(
-    pet => pet.petID < 50 //will locate the first intanse of petID less than 50
+  pet => pet.petID < 50 //will locate the first intanse of petID less than 50
+  //it stopped its execution on the array index of 1, on petID: 43
 );
-console.log(pet) //{ petID: 43, cat: 'small', color: 'orange' }
+console.log(pet); //{ petID: 43, cat: 'small', color: 'orange' }
+
+
+//Find the index of any element that you choose
+const index = animalID.findIndex(
+    indexNum => indexNum.petID === 378 //index of petID: 378 is number 2
+)
+console.log(index);
