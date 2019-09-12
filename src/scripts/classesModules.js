218ae2f8208bcee62b1f838c0af4b0317ff33b14
IngridGdesigns <aleja.morada@gmail.com>
2019-09-11 17:38:20 -0700

    added class basics example

---
+++ src/scripts/classesModules.js
@@ -0,0 +1,35 @@
+//Classes and Modules!!
+/* Classes give us a new syntax to create constructor functions, and Modules is a great way to organize our source code */
+
+/* Example 1 */
+//Class Basics 
+//class keyword to create a class by convention the first letter is in uppercase
+
+class Beverage {
+
+}
+let water = new Beverage();
+console.log(water);
+////////////////////////////////////////////////////////////////
+
+//Constructors and Properties
+
+/* Example 1 */
+
+////////////////////////////////////////////////////////////////
+
+//Methods
+
+/* Example 1 */
+
+////////////////////////////////////////////////////////////////
+
+//Inheritance
+
+/* Example 1 */
+
+////////////////////////////////////////////////////////////////
+
+//Creating a module
+
+/* Example 1 */
