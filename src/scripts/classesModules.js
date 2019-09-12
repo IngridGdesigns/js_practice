459f8321004349a5e98ffe6759a1963ea2029641
IngridGdesigns <aleja.morada@gmail.com>
2019-09-11 17:44:39 -0700

    Constructor example and notes

--- src/scripts/classesModules.js
+++ src/scripts/classesModules.js
@@ -6,15 +6,27 @@
 //class keyword to create a class by convention the first letter is in uppercase
 
 class Beverage {
-
-}
+//instantiating a class
+    
+};
 let water = new Beverage();
 console.log(water);
 ////////////////////////////////////////////////////////////////
 
 //Constructors and Properties
+//a constructor is a function that gets executed when a new instance
+//of an object gets created
 
 /* Example 1 */
+class Temperature {
+    //instantiating a class
+        constructor(id){
+            this.id = id; //this keyword is needed
+        }
+    };
+    let cool = new Temperature('cool');
+    cool.id = 123;
+    console.log(cool.id);
 
 ////////////////////////////////////////////////////////////////
 
